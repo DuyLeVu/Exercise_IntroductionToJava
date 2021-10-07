632530ed649a18f56167708ecc6ebf6d67a65cad
DuyLeVu <levuduy132@gmail.com>
2021-10-07 11:05:36 +0700

    Add Change Current

---
+++ ChangeCurrent/src/ChangeCurrent.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ChangeCurrent {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double usd;
+        double rate = 23000;
+        do {
+            System.out.println("Enter USD: ");
+            usd = sc.nextDouble();
+        } while (usd < 0);
+
+         double result = usd * rate;
+        System.out.printf("'%.2f' USD = %.2f VND ",usd ,result);
+    }
+}
